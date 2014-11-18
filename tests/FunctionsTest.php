2452edde344f4bcd4087898b4ad63eb12333dcd5
Evert Pot <evert@rooftopsolutions.nl>
2014-11-17 23:44:23 -0500

    Another PHP 5.3 tweak.

--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -52,52 +52,52 @@ class FunctionsTest extends \PHPUnit_Framework_TestCase{
 
     function resolveData() {
 
-        return [
-            [
+        return array(
+            array(
                 'http://example.org/foo/baz',
                 '/bar',
                 'http://example.org/bar',
-            ],
-            [
+            ),
+            array(
                 'https://example.org/foo',
                 '//example.net/',
                 'https://example.net/',
-            ],
-            [
+            ),
+            array(
                 'https://example.org/foo',
                 '?a=b',
                 'https://example.org/foo?a=b',
-            ],
-            [
+            ),
+            array(
                 '//example.org/foo',
                 '?a=b',
                 '//example.org/foo?a=b',
-            ],
+            ),
             // Ports and fragments
-            [
+            array(
                 'https://example.org:81/foo#hey',
                 '?a=b#c=d',
                 'https://example.org:81/foo?a=b#c=d',
-            ],
+            ),
             // Relative.. in-directory paths
-            [
+            array(
                 'http://example.org/foo/bar',
                 'bar2',
                 'http://example.org/foo/bar2',
-            ],
+            ),
             // Now the base path ended with a slash
-            [
+            array(
                 'http://example.org/foo/bar/',
                 'bar2/bar3',
                 'http://example.org/foo/bar/bar2/bar3',
-            ],
+            ),
             // .. and .
-            [
+            array(
                 'http://example.org/foo/bar/',
                 '../bar2/.././/bar3/',
                 'http://example.org/foo/bar3/',
-            ],
-        ];
+            ),
+        );
 
     }
 
