76893bcc15945074e87cb77e7621c20762228323
Evert Pot <me@evertpot.com>
2016-10-07 00:08:40 -0400

    sabre/cs 1.0

--- tests/BuildTest.php
+++ tests/BuildTest.php
@@ -19,19 +19,19 @@ class BuildTest extends \PHPUnit_Framework_TestCase{
     function buildUriData() {
 
         return [
-            [ 'http://example.org/'],
-            [ 'http://example.org/foo/bar'],
-            [ '//example.org/foo/bar'],
-            [ '/foo/bar'],
-            [ 'http://example.org:81/'],
-            [ 'http://user@example.org:81/'],
-            [ 'http://example.org:81/hi?a=b'],
-            [ 'http://example.org:81/hi?a=b#c=d'],
+            ['http://example.org/'],
+            ['http://example.org/foo/bar'],
+            ['//example.org/foo/bar'],
+            ['/foo/bar'],
+            ['http://example.org:81/'],
+            ['http://user@example.org:81/'],
+            ['http://example.org:81/hi?a=b'],
+            ['http://example.org:81/hi?a=b#c=d'],
             // [ '//example.org:81/hi?a=b#c=d'], // Currently fails due to a
             // PHP bug.
-            [ '/hi?a=b#c=d'],
-            [ '?a=b#c=d'],
-            [ '#c=d'],
+            ['/hi?a=b#c=d'],
+            ['?a=b#c=d'],
+            ['#c=d'],
         ];
 
     }
