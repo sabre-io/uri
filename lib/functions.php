d02f5cc39b2f762a48fec3c816536d7f6d807670
Phil Davis <phil@jankaritech.com>
2021-11-03 16:49:56 +0545

    Enhance PHPdoc for parts param to build() function

--- lib/functions.php
+++ lib/functions.php
@@ -214,7 +214,7 @@ function parse(string $uri): array
  * This function takes the components returned from PHP's parse_url, and uses
  * it to generate a new uri.
  *
- * @param array<string, int|string> $parts
+ * @param array<string, int|string|null> $parts
  */
 function build(array $parts): string
 {
