6632e7d6a4318e27779fd7caffe6250f6c6ad872
Christoph Wurst <1374172+ChristophWurst@users.noreply.github.com>
2026-03-26 14:28:39 +0100

    chore(deps): add rector
    Signed-off-by: Christoph Wurst <1374172+ChristophWurst@users.noreply.github.com>

---
+++ rector.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/lib',
+        __DIR__ . '/tests',
+    ])
+    ->withPhpSets(php74: true,)
+    ->withTypeCoverageLevel(0)
+    ->withDeadCodeLevel(0)
+    ->withCodeQualityLevel(0);
